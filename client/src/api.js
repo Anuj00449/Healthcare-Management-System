0ae8ccafdd1e8fabbd7d55a05df925b36bef0126
Anuj Kumar <girianuj140@gmail.com>
2026-06-09 13:52:54 +0530

    update backend api url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://healthcare-management-system-678q.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
